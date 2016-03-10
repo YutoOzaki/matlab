781048c659950fa43d7cd42576c092cfef528a5e
YutoOzaki <yutoozaki@hotmail.com>
2016-03-11 07:12:58 +0900

    Added files via upload

--- src/dev/AFE/AudioFeatureExtraction.m
+++ src/dev/AFE/AudioFeatureExtraction.m
@@ -1,38 +1,68 @@
 function [dataMat,dataLabel] = AudioFeatureExtraction(dataSet,hprms)    
     %% read hyperparameters
     method      = hprms.method;
-    d           = hprms.d;
-    dd          = hprms.d;
-    dim         = length(hprms.coef_range);
-    blocks      = hprms.blocks;
-    patch       = hprms.patch;
     checkfs     = hprms.checkfs;
-    
-    hprms.w     = hprms.w(hprms.N);
-    
     
     path        = dataSet.dataPath;
     label       = dataSet.dataLabel;
     samples     = length(path);
-
-    %% implement audio feature extractor
+    
     featureExtractor = cell(1,1);
+    
+    %% implement audio feature extractor
     if strcmp(method,'MFCC')
         featureExtractor{1} = @MFCC;
+        
+        %% matrix for storing audio features
+        coefnum     = hprms.coefnum;
+        dim         = length(hprms.coef_range);
+        d           = hprms.d;
+        dd          = hprms.d;
+        blocks      = hprms.blocks;
+        patch       = hprms.patch;
+        
+        if d == true && dd == false
+            dim = dim * 2;
+        elseif d == true && d == true
+            dim = dim * 3;
+        end
+        
+        dataMat     = zeros(dim,samples*patch,blocks);
+        dataLabel   = zeros(1,samples*patch,blocks);
+        
+        %% liftering vector
+        liftcoef    = hprms.liftcoef;
+        liftering   = hprms.liftering;
+        if liftering == 1
+            hprms.ceplifter = 1 + (liftcoef/2)*sin(pi*(1:coefnum)'/liftcoef);
+        elseif liftering == 2
+            a = liftcoef(1);
+            tau = liftcoef(2);
+            hprms.ceplifter = ((1:coefnum).^a).*exp(-((1:coefnum).^2)./(2*tau^2));
+        else
+            hprms.ceplifter = ones(coefnum,1);
+        end
+        
+        %% mel-filter bank
+        mfb_type    = hprms.mfb_type;
+        f_start     = hprms.f_start;
+        f_end       = hprms.f_end;
+        unitpow     = hprms.unitpow;
+        FFTL        = 2^nextpow2(hprms.N);
+        FFTL_half   = 1:(floor(FFTL/2) + 1);
+        if mfb_type == 1
+            hprms.melfilbank = triangularFB(coefnum,f_start,f_end,FFTL,unitpow);
+        else
+            melfilbank = mfccFB40(FFTL);
+            hprms.melfilbank = melfilbank(FFTL_half,:)';
+        end
+    
+        %% window function
+        hprms.w     = hprms.w(hprms.N);
     elseif strcmp(method,'CR')
         featureExtractor{1} = @CR;
     end
     
-    %% prepare variables
-    if d == true && dd == false
-        dim = dim * 2;
-    elseif d == true && d == true
-        dim = dim * 3;
-    end
-    
-    dataMat = zeros(dim,samples*patch,blocks);
-    dataLabel = zeros(1,samples*patch,blocks);
-
     %% extract audio features
     for i=1:samples
         pathBuf = strrep(path{i},'\','/');
@@ -43,6 +73,9 @@ function [dataMat,dataLabel] = AudioFeatureExtraction(dataSet,hprms)
         if fs ~= checkfs
             warning(' Unexpected sampling frequency (%d)',fs);
         end
-        %featureExtractor{1}(x,fs,hprms,label(i));
+        
+        idx = (i-1)*patch+1:i*patch;
+        dataLabel(1,idx,:) = label(i);
+        %dataMat(:,idx,:) = featureExtractor{1}(x,hprms);
     end
 end
--- src/dev/AFE/MFCC.m
+++ src/dev/AFE/MFCC.m
@@ -1,13 +1,6 @@
-function MFCC(x,fs,hprms,label)
+function MFCC(x,hprms,label)
     %% get hyperparameters
-    mfb_type    = hprms.mfb_type;
-    f_start     = hprms.f_start;
-    f_end       = hprms.f_end;
-    coefnum     = hprms.coefnum;
     coef_range  = hprms.coef_range;
-    unitpow     = hprms.unitpow;
-    liftering   = hprms.liftering;
-    liftcoef    = hprms.liftcoef;
     mfbc        = hprms.mfbc;
     d           = hprms.d;
     dd          = hprms.dd;
@@ -20,27 +13,7 @@ function MFCC(x,fs,hprms,label)
     normalize   = hprms.normalize;
     zcaeps      = hprms.zcaeps;
     
-    %% calculate internal parameters
-    FFTL = 2^nextpow2(N);
-    FFTL_half = 1:(floor(FFTL/2) + 1);
-
-    if liftering == 1
-        ceplifter = 1 + (liftcoef/2)*sin(pi*(1:coef)'/liftcoef);
-    elseif liftering == 2
-        a = liftcoef(1);
-        tau = liftcoef(2);
-        ceplifter = ((1:coef).^a).*exp(-((1:coef).^2)./(2*tau^2));
-    else
-        ceplifter = ones(coef,1);
-    end
-
-    %% get filter bank
-    if mfb_type == 1
-        trifil = triangularFB(coefnum,f_start,f_end,FFTL,unitpow);
-    else
-        trifil = mfccFB40(FFTL,f_s);
-        trifil = trifil(FFTL_half,:)';
-    end
+    ceplifter   = hprms.ceplifter;
 
     %% get block-wise features
     [testMat,testLabel,testSourceInfo] = blockWiseMFCC(testURL,mapObj,coef_range,blocks,patch,N,M,preemp,w,trifil,ceplifter,isdct);
