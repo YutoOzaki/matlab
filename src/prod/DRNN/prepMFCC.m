284aada6f92c9279fc5298b60aabd759604976ad
YutoOzaki <yutoozaki@hotmail.com>
2016-04-27 01:04:47 +0900

    Delete prepMFCC.m

--- src/prod/DRNN/prepMFCC.m
+++
@@ -1,9 +0,0 @@
-function [trainMat,testMat,trainLabel,testLabel] = prepMFCC(MFCC_data)
-    load(MFCC_data);
-    
-    trainLabel = trainLabel + 1;
-    testLabel = testLabel + 1;
-    
-    trainMat = permute(trainMat,[2 1 3]);
-    testMat = permute(testMat,[2 1 3]);
-end
